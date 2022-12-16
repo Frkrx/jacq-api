d2dcb9efe7bf63fb448bcd213d677fa0e12bc415
Frank <frank.20122558@gmail.com>
2022-12-16 20:18:46 +0100

    Creation of a new project api on intelij with spring. The project will be an api rest for an Angular Front of an application chat slack like.

---
+++ jacq-api/src/main/java/com/m2i/jacqapi/JacqApiApplication.java
@@ -0,0 +1,13 @@
+package com.m2i.jacqapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacqApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JacqApiApplication.class, args);
+	}
+
+}
